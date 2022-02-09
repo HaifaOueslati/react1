954d0e4b37f1533e3cea9d81c9267964e7889c46
algo3 <haifaoueslati50@gmail.com>
2022-02-09 16:18:58 +0100

    reactjscheckpoint

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Form1 from './Components/Form';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <Form1/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
